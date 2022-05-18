4dfb6efc0fd43a1a2b6f8671406ff3bd9c644fd6
Karthik K <karthik74118@gmail.com>
2022-05-18 09:23:02 +0530

    Registration Form Project Complete

--- Registration Form/index.js
+++ Registration Form/index.js
@@ -5,10 +5,25 @@ function validate() {
   let phoneNumber = document.getElementById("phoneNumber").value;
   let state = document.getElementById("state").value;
   let zipCode = document.getElementById("zipCode").value;
+  let tncCheckBox = document.getElementById("tncCheck").checked;                  //For checking the value of uncheck and check
+
+
+
+
   let firstNameInvalid = document.getElementById("firstNameInvalid")
   let firstNameValid = document.getElementById("firstNameValid")
   let lastNameInvalid = document.getElementById("lastNameInvalid")
   let lastNameValid = document.getElementById("lastNameValid")
+  let emailValid = document.getElementById("emailValid")
+  let emailInvalid = document.getElementById("emailInvalid")
+  let phoneNumberValid = document.getElementById("phoneNumberValid")
+  let phoneNumberInvalid = document.getElementById("phoneNumberInvalid")
+  let zipCodeValid = document.getElementById("zipCodeValid")
+  let zipCodeInvalid = document.getElementById("zipCodeInvalid")
+  let tncInvalid = document.getElementById("tncInvalid")
+  let stateValid = document.getElementById("stateValid")
+  let stateInvalid = document.getElementById("stateInvalid")
+
 
   console.log(firstName, lastName, email, phoneNumber, state, zipCode);
 
@@ -26,5 +41,43 @@ function validate() {
   } else {
     lastNameValid.style.display = "block"
     lastNameInvalid.style.display ="none"
+  }
+
+  if (email === '' || !email.includes("@") || email.startsWith("@") || !email.includes(".") || email.slice(email.lastIndexOf(".")+1).length < 2) {
+    emailInvalid.style.display = "block"
+    emailValid.style.display = "none"
+  } else {
+    emailValid.style.display = "block"
+    emailInvalid.style.display = "none"
+  }
+
+  if (phoneNumber === '' || phoneNumber.length != 10 || Number(phoneNumber[0]) < 6) {
+    phoneNumberInvalid.style.display = "block"
+    phoneNumberValid.style.display ="none"
+  } else {
+    phoneNumberValid.style.display = "block"
+    phoneNumberInvalid.style.display ="none"
+  }
+
+  if (zipCode === '' || zipCode.length != 6) {
+    zipCodeInvalid.style.display = "block"
+    zipCodeValid.style.display ="none"
+  } else {
+    zipCodeValid.style.display = "block"
+    zipCodeInvalid.style.display ="none"
+  }
+
+  if (!tncCheckBox) {
+    tncInvalid.style.display = "block"
+  } else {
+    tncInvalid.style.display = "none"
+  }
+
+  if (state === "") {
+    stateInvalid.style.display = "block"
+    stateValid.style.display = "none"
+  } else {
+    stateValid.style.display = "block"
+    stateInvalid.style.display = "none"
   }
 }
