b5a0f0ddbe0dc413efe43cf9ab84475d1aadbe44
Karthik K <karthik74118@gmail.com>
2022-05-27 10:13:04 +0530

    Tic Tac Toe Project Complete and added to head

--- TicTacToe/index.js
+++ TicTacToe/index.js
@@ -1,7 +1,12 @@
 let grids = document.getElementsByClassName("col")
 let currentTurnText = document.getElementById("currentTurnText")
 let currentTurn = 1
+let gameOver = false
 
+const playerTicker = {
+    1: 'X',
+    2: 'O'
+}
 const winningCombinations = [
     [0, 1, 2],
     [3, 4, 5],
@@ -15,14 +20,23 @@ const winningCombinations = [
 
 
 const singleMove = (index) => {
-    if (!grids[index].innerText) {
-        grids[index].innerText = currentTurn === 1 ? "X" : "O"
+    if (!gameOver && !grids[index].innerText) {
+        grids[index].innerText = currentTurn === 1 ? playerTicker[1] : playerTicker[2]
         let winningPlayer = checkWinningStatus()
-        if (winningPlayer) {
-            alert(winningPlayer === "X" ? "Player 1 Won!" : "Player 2 Won!")
+        if (winningPlayer != 'No Winner') {
+            gameOver = true
+            currentTurnText.innerText = `Player ${winningPlayer} won!`
+            currentTurnText.style.backgroundColor = '#90EE90'
+            return
         }
-        currentTurn = (currentTurn === 1 ? 2 : 1)
+        currentTurn = currentTurn === 1 ? 2 : 1
         currentTurnText.innerText = `Player ${currentTurn}'s turn`
+        
+        if (winningPlayer == 'No Winner' && combination.every(index => grids[index].innerText)) {
+            currentTurnText.innerText = "Match Drawn"
+            currentTurnText.style.backgroundColor = "#808080"
+            return
+        }
     }
 
 }
@@ -30,13 +44,19 @@ const singleMove = (index) => {
 const checkWinningStatus = () => {
     for (let i = 0; i < winningCombinations.length; i++){
         combination = winningCombinations[i]
-        if (grids[combination[0]].innerText === grids[combination[1]].innerText && grids[combination[1]].innerText === grids[combination[2]].innerText && grids[combination] !== '') {
-            return grids[combination[0]].innerText
+        let value = grids[combination[0]].innerText
+        if (combination.every(index => grids[index].innerText && grids[index].innerText === value)) {
+            addBorderStyle(combination)
+            return value === 'X' ? 1 : 2
         }
     }
-    return 
+    return 'No Winner'
 }
 
+
+const addBorderStyle = (indexArray) => {
+    indexArray.forEach(index => grids[index].style.cssText = 'background-color: rgba(144,238,144,1)')
+}
 
 
 
