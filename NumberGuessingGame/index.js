fc81cf4748af17df811b22023281afe908c85f63
Karthik K <karthik74118@gmail.com>
2022-07-09 14:36:04 +0530

    Updated Number Guessing Game and Initiated To Do List

--- NumberGuessingGame/index.js
+++ NumberGuessingGame/index.js
@@ -1,30 +1,36 @@
 let randomNumber = Math.trunc(Math.random() * 100);
 let chancesLeft = 10;
-let footerDiv = document.getElementsByClassName("footer-Div");
-let footerP = document.createElement("p");
+document.getElementById("hint").style.display = "none"
 document.getElementById("chancesLeft").innerText =
   "Chances Left: " + chancesLeft;
 function verify() {
   --chancesLeft;
+  document.getElementById("chancesLeft").innerText =
+      "Chances Left: " + chancesLeft;
   if (chancesLeft > 0) {
     let inputNumber = document.getElementById("input-number").value;
 
     document.getElementById("lastGuess").innerText += " " + inputNumber + " ";
-    document.getElementById("chancesLeft").innerText =
-      "Chances Left: " + chancesLeft;
+    
     if (inputNumber == randomNumber) {
       document.getElementById("hint").innerText = "You guessed the number!";
-
+      document.getElementById("hint").style.display = "block"
+      document.getElementById("hint").classList = 'success'
       return;
     } else if (inputNumber > randomNumber) {
-      document.getElementById("hint").innerText =
-        "Too High! Choose a lower number";
+      document.getElementById("hint").innerText =  "Too High! Choose a lower number";
+      document.getElementById("hint").style.display = "block" 
+      document.getElementById("hint").classList = 'danger'
+        
     } else {
-      document.getElementById("hint").innerText =
-        "Too Low! Choose a higher number";
+      document.getElementById("hint").innerText = "Too Low! Choose a higher number";
+      document.getElementById("hint").style.display = "block"  
+      document.getElementById("hint").classList = 'danger'
     }
   } else {
-    document.getElementById("hint").innerText =
-      "Your chances are over! Refresh for a new game";
+    document.getElementById("hint").innerText = "Your chances are over! Refresh for a new game";
+    document.getElementById("hint").style.display = "block"
+    document.getElementById("hint").classList = 'gameOver'
+    document.getElementById("submit-button").disabled = true;
   }
 }
