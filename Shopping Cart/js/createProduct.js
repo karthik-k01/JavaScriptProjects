5cb5a4cbe3483cc537e52c376d526915b7b0591c
Karthik K <karthik74118@gmail.com>
2022-08-01 07:24:01 +0530

    Dynamic creation of divs and fetched data from API

--- Shopping Cart/js/createProduct.js
+++ Shopping Cart/js/createProduct.js
@@ -1,13 +1,13 @@
-let parentDiv = document.getElementById('products-display')
+const parentDiv = document.getElementById('products-display')
 
-const productDiv = () => {
+const createProductDiv = (product) => {
     let colDiv = document.createElement('div')
     let cardProductDiv = document.createElement('div')
     let cardImage = document.createElement('img')
     let cardBodyDiv = document.createElement('div')
     let productHeaderDiv = document.createElement('div')
-    let cardTitleH5 = document.createElement('h5')
-    let cardTitlePriceH4 = document.createElement('h4')
+    let cardTitleH5 = document.createElement('h6')
+    let cardTitlePriceH4 = document.createElement('h5')
     let cardDescriptionP = document.createElement('p')
     let starSpan1 = document.createElement('span')
     let starSpan2 = document.createElement('span')
@@ -38,9 +38,56 @@ const productDiv = () => {
     cardBodyDiv.appendChild(outerBtnDiv)
     outerBtnDiv.appendChild(AddToCartBtn)
     outerBtnDiv.appendChild(BuyNowBtn)
+
+    colDiv.classList = "col-md-3"
+    cardProductDiv.classList = "card product"
+    cardImage.classList = "card-img-top"
+    
+    cardBodyDiv.classList = "card-body"
+    productHeaderDiv.classList = "product-header"
+    cardTitleH5.classList = "card-title"
+    cardTitlePriceH4.classList = "card-title price-title"
+    cardDescriptionP.classList = "card-text"
+    starSpan1.classList = "fa fa-star checked"
+    starSpan2.classList = "fa fa-star checked"
+    starSpan3.classList = "fa fa-star checked"
+    starSpan4.classList = "fa fa-star checked"
+    starSpan5.classList = "fa fa-star checked"
+    outerBtnDiv.classList = "btn-outer"
+    AddToCartBtn.classList = "btn btn-primary"
+    BuyNowBtn.classList = "btn btn-primary btn-buy"
+
+    colDiv.id = `product-${product.id}`
+    cardDescriptionP.innerText = `${product.description.slice(0,50)}...`
+    cardImage.src = product.image
+    cardTitlePriceH4.innerText = `$${product.price}`
+    cardTitleH5.innerText = `${product.title.slice(0,20)}...`
+    numberOfRatingsSpan.innerText = `(${product.rating.count})`
+
+    // cardTitleH5.innerText = "Product 1"
+    // cardTitlePriceH4.innerText = "$123"
+    // cardDescriptionP.innerText = "Some quick example text to build on the card title"
+    // numberOfRatingsSpan.innerText = "(57)"
+    AddToCartBtn.innerText = "Add to Cart"
+    BuyNowBtn.innerText = "Buy Now"
+
+    // cardImage.src = "https://fakestoreapi.com/img/71-3HjGNDUL._AC_SY879._SX._UX._SY._UY_.jpg"
+    // cardImage.alt = "Image"
 }
 
-<div class="col-md-3">
+// category: "men's clothing"
+// description: "Your perfect pack for everyday use and walks in the forest. Stash your laptop (up to 15 inches) in the padded sleeve, your everyday"
+// id: 1
+// image: "https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg"
+// price: 109.95
+// rating:
+// count: 120
+// rate: 3.9
+// [[Prototype]]: Object
+// title: "Fjallraven - Foldsack No. 1 Backpack, Fits 15
+
+
+{/* <div class="col-md-3">
                 <div class="card product">
                     <img src="https://fakestoreapi.com/img/71-3HjGNDUL._AC_SY879._SX._UX._SY._UY_.jpg" class="card-img-top" alt="...">
                     <div class="card-body">
@@ -64,4 +111,4 @@ const productDiv = () => {
                         
                     </div>
                 </div>
-            </div>
+            </div> */}
--- Shopping Cart/js/index.js
+++ Shopping Cart/js/index.js
@@ -1,6 +1,15 @@
-const fetchData = () => {
-    fetch('https://fakestoreapi.com/products')
-        .then(res => { return res.json() })
-        .then(data => console.log(data))
-            .catch(err => console.log(err))
+const fetchData = async () => {
+    const res = await fetch('https://fakestoreapi.com/products')
+    const data = await res.json()
+    console.log(data)
+    
+    
+    data.forEach(product => createProductDiv(product))
 }
+
+
+
+// fetch('https://fakestoreapi.com/products')
+//         .then(res => { return res.json() })
+//         .then(data => console.log(data))
+//         .catch(err => console.log(err))
